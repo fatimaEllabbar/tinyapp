d7e01270d4a3d9bb55034c35bc92c42b845120c0
vagrant <fatima.ellabbar@gmail.com>
2020-12-07 23:07:53 +0000

    rendring information about a single url

--- express_server.js
+++ express_server.js
@@ -7,28 +7,26 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// adding a route to urls using template engine
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Render information about a single URL
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL : req.params.shortURL, longURL : urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+
+
+
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
 
 app.listen(PORT, () => {

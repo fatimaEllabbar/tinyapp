5e3409e9816fea0305787db12abd43fcfb2d633c
vagrant <fatima.ellabbar@gmail.com>
2020-12-08 00:15:41 +0000

    handling post request

--- express_server.js
+++ express_server.js
@@ -3,6 +3,15 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs") ;
 
+//using the body-parser library to make the POST request body human readable
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// generating a "unique" shortURL by returning a string of 6 random alphanumeric characters
+function generateRandomString() {
+  return Math.random().toString(36).substring(2,8);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,15 +23,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//add a get route to show the form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+});
+
 //Render information about a single URL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL : req.params.shortURL, longURL : urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+//created a route to handle the POST requests from our form
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+
 
 
 

8e7affeeab2b400e0040e9f0682b31516e44c2d3
vagrant <fatima.ellabbar@gmail.com>
2020-12-09 19:46:22 +0000

    adding login and register to tinyapp

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const app = express();
 const PORT = 8080; // default port 8080
+
+const app = express();
+
 app.set("view engine", "ejs") ;
+
 const  cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
@@ -19,23 +22,80 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//log in and create a cookie
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+//adding registration 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    user : users[req.cookies["user_id"]]
+   };
+  res.render("registration",templateVars);
+})
+// lookup for an email
+const emailExists = (email) => {
+  for (let key in users) {
+    if (users[key].email === email) {
+      return users[key];
+    }
+  }
+  return null;
+} 
+app.post("/register", (req, res) => {
+  if(req.body.email && req.body.password && emailExists(req.body.email) === null){
+    const userRandomID = generateRandomString();
+    users[userRandomID] = {
+      id : userRandomID,
+      email : req.body.email,
+      password : req.body.password
+    };
+    res.cookie("user_id", users[userRandomID].id);
+    res.redirect("/urls");
+  } else {
+    res.statusCode = 400;
+    res.sendStatus(res.statusCode);
+  }
+});
+
+//log in 
+app.get("/login", (req, res) =>{
+  let templateVars = {
+    user : users[req.cookies["user_id"]]
+   };
+  res.render("login",templateVars);
+});
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username",username);
-  res.redirect("/urls")
+  const user = emailExists(req.body.email);
+  if(user && user.password === req.body.password) {
+    res.cookie("user_id",user.id);
+    res.redirect("/urls");
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 //log out and clear the cookie
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls")
 });
+
+
 // adding a route to urls using template engine
 app.get("/urls", (req, res) => {
   let templateVars = {
      urls: urlDatabase, 
-     username : req.cookies["username"]
+     user : users[req.cookies["user_id"]]
     };
 
  
@@ -46,7 +106,7 @@ app.get("/urls", (req, res) => {
 //add a get route to show the form
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username : req.cookies["username"]
+    user : users[req.cookies["user_id"]]
    };
   res.render("urls_new",templateVars)
 });
@@ -55,7 +115,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL : req.params.shortURL,
      longURL : urlDatabase[req.params.shortURL],
-     username : req.cookies["username"]
+     user : users[req.cookies["user_id"]]
      };
   res.render("urls_show", templateVars);
 });
